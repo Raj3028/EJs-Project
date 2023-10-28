0e23c8c3a27b4c9a9fb81dc8e00a5f25bcbc4f76
Raj <rajeshkumarsahoo006@gmail.com>
2023-10-28 18:24:31 +0530

    Initial Push

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(cookieParser());
 app.use(session({ secret: 'vaibhav_1234', resave: false, saveUninitialized: false }));
